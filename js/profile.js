5781be3341462ac5fb609b24f77c4d62f2d197c6
PengYu Chen <118327997+F0ste@users.noreply.github.com>
2024-03-31 18:53:18 +0800

    Initial commit

---
+++ js/profile.js
@@ -0,0 +1,15 @@
+
+const buttons = document.querySelectorAll('.nav-button');
+
+
+buttons.forEach(button => {
+  button.addEventListener('mouseover', () => {
+    button.classList.add('animated');
+  });
+
+  button.addEventListener('mouseout', () => {
+    button.classList.remove('animated');
+  });
+});
+
+
